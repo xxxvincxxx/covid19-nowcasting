916ad2cf81002de63775e1d14e55c9858d58c64f
xxxvincxxx <vincenzo.grass@gmail.com>
2021-10-30 19:45:09 +0200

    VERY IMPORTANT! read googlesheet from drive via ID in R, no other stuff needed

--- main.R
+++ main.R
@@ -1,2 +1,6 @@
 library(tidyverse)
 library(janitor)
+
+## load data
+id <- "1TLNATbUPRpAmMosvERvOJlDYirc4EHIv" # google file ID
+df <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
