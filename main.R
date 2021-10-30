4bb8758cd4e027da8375bfbc6f8bbf10dc44e302
xxxvincxxx <vincenzo.grass@gmail.com>
2021-10-30 19:47:19 +0200

    VERY IMPORTANT! read googlesheet from drive via ID in R, no other stuff needed

--- main.R
+++ main.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(janitor)
 
-## load data
+## load data from googledoc;
+## the document comes from FKI covid19 data up to July 2020
+## (I was still in Berlin back then, wondering around Tempelhof)
 id <- "1TLNATbUPRpAmMosvERvOJlDYirc4EHIv" # google file ID
 df <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
